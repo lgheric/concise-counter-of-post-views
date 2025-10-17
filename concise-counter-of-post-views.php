abee955ce42d2242e6ab150e1beda897d9def2f6
Robert South <robert2021south@gmail.com>
2025-10-17 15:54:25 +0800

    Update Author name to RobertWP\(Robert South\)

--- concise-counter-of-post-views.php
+++ concise-counter-of-post-views.php
@@ -10,7 +10,7 @@
  * ✅ Shortcode [concise_views] to display the view count anywhere
  * Ideal for bloggers and content creators who want simple and accurate view tracking without bloat.
  * Version: 3.1
- * Author: Robert South
+ * Author: RobertWP (Robert South)
  * Author URI: https://robertwp.com
  * License: GPLv3 or later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
